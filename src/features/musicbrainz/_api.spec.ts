1a816c3276bf2f3067ccf8de7b0605a30b365e53
Nyarlathotep <anhedonia@skiff.com>
2024-05-17 05:17:18 -0400

    feat(tests): update musicbrainz

--- src/features/musicbrainz/_api.spec.ts
+++ src/features/musicbrainz/_api.spec.ts
@@ -1,8 +1,9 @@
-import {musicbrainzApi} from '@features/musicbrainz/artist/artiest.api.ts'
+import {musicbrainzApi} from '@features/musicbrainz/musicbrainz.api.ts'
 import {configureStore} from '@reduxjs/toolkit'
 import {Effect} from 'effect'
 import {beforeEach, describe, expect, test} from 'vitest'
 import {queryBuilder} from './effects/queries'
+import type {ArtistFields} from './artist/artist.fields'
 
 const makeStore = () =>
 	configureStore({
@@ -19,25 +20,33 @@ describe('@musicbrainz/api', () => {
 	})
 
 	test('query', async () => {
-		const {getArtistsByAlias} = musicbrainzApi.endpoints
+		const artistQuery = musicbrainzApi.endpoints.artist
 		const {dispatch, getState} = store
-		const query = '"Hoshimachi,+Suisei"'
+		const query: ArtistFields = {artist: '"Hoshimachi,+Suisei"'}
 
-		await dispatch(getArtistsByAlias.initiate(query))
-		const {data} = getArtistsByAlias.select(query)(getState())
+		await dispatch(artistQuery.initiate(query))
+		const {data} = artistQuery.select(query)(getState())
 
 		expect(data?.[0].country).equal('JP')
 	})
 })
 
-describe('effect', () => {
-	test('builQuery', async () => {
+describe('query-builders', () => {
+	test('args#1', () => {
+		const program = queryBuilder({artist: '星街すいせい'})
+		const result = Effect.runSync(program)
+		expect(result).toBe('artist:"星街すいせい"')
+	})
+
+	test('args#3', () => {
 		const program = queryBuilder({
 			title: 'ビビデバ',
 			artist: '星街すいせい',
 			country: 'XW'
 		})
-
-		await Effect.runPromise(program).then(console.log)
+		const result = Effect.runSync(program)
+		expect(result).toBe(
+			'title:"ビビデバ" AND artist:"星街すいせい" AND country:"XW"'
+		)
 	})
 })
