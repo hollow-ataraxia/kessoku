2705d39c057b0bb9f184d2da832770d64d19f598
Nyarlathotep <anhedonia@skiff.com>
2024-05-17 06:56:09 -0400

    feat(folder structure): rename redux-store

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import {store} from '@features/redux-store/store.ts'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
 import App from './App.tsx'
+import {store} from './redux-store/store.ts'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') ?? document.body).render(
