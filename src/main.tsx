7e31ebb528d198ee682db40ea991aa586485d370
Nyarlathotep <anhedonia@skiff.com>
2024-05-19 19:55:33 -0400

    chore: fmt

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import '@styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
 import App from './App.tsx'
 import {store} from './redux-store/store.ts'
-import '@styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') ?? document.body).render(
 	<React.StrictMode>
